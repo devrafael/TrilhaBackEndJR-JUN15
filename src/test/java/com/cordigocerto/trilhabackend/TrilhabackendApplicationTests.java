fd1360755b53c053fa2359af5877dcb9f059b6e7
devrafael <almeida9137@gmail.com>
2024-07-10 09:56:47 -0300

    Iniciando o projeto

---
+++ src/test/java/com/cordigocerto/trilhabackend/TrilhabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordigocerto.trilhabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilhabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

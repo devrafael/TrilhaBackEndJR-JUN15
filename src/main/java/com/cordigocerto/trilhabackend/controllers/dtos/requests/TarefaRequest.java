5378b8a7fcd5e0bb368de63676506b8c81ab3ed0
devrafael <almeida9137@gmail.com>
2024-08-02 14:31:02 -0300

    Atualização do DTO TarefaRequest

--- src/main/java/com/cordigocerto/trilhabackend/controllers/dtos/requests/TarefaRequest.java
+++ src/main/java/com/cordigocerto/trilhabackend/controllers/dtos/requests/TarefaRequest.java
@@ -1,5 +1,5 @@
 package com.cordigocerto.trilhabackend.controllers.dtos.requests;
 
-public record TarefaRequest(String tarefaDescricao, Long usuario_id) {
+public record TarefaRequest(String tarefaDescricao) {
 
 }
